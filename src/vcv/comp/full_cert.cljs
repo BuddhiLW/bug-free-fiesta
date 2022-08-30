81db1420e962648ae33124694a96795cc2584467
BuddhiLW <pedrogbranquinho@gmail.com>
2022-08-29 23:45:44 -0300

    Responsive modal-design for wide-screen (xxl).

--- src/vcv/comp/full_cert.cljs
+++ src/vcv/comp/full_cert.cljs
@@ -3,15 +3,15 @@
 
 (defn mini-cert
   [id header img title descr link date values modal]
-  [:div.col
+  [:div.col-xxl.align-self-center
    [:div.card.shadow {:key id}
     [:h1.card-header.row.align-items
      [:div.col header]
      [:button.btn-close
-        {:on-click #(do (helpers/toggle-modal {:active false} modal)
-                        (reset! values {:id (str "cert-" (random-uuid)) :header "" :title ""
-                                         :img "" :institution "" :platform "" :descr "" :tags ""
-                                         :link "" :date ""}))}]]
+      {:on-click #(do (helpers/toggle-modal {:active false} modal)
+                      (reset! values {:id (str "cert-" (random-uuid)) :header "" :title ""
+                                      :img "" :institution "" :platform "" :descr "" :tags ""
+                                      :link "" :date ""}))}]]
 
     [:div.shadow-sm.mb-1.bg-body.rounded
      [:img.card-img-top.resize {:src img
@@ -25,8 +25,8 @@
      [:button.btn.btn-secondary.col-3
       {:on-click #(do (helpers/toggle-modal {:active false} modal)
                       (reset! values {:id (str "cert-" (random-uuid)) :header "" :title ""
-                                           :img "" :institution "" :platform "" :descr "" :tags ""
-                                           :link "" :date ""}))}
+                                      :img "" :institution "" :platform "" :descr "" :tags ""
+                                      :link "" :date ""}))}
       "Back to main page"]]]])
 
 (defn full-cert
@@ -35,5 +35,5 @@
    [:div.modal-dialog.modal-fullscreen
     [:div.modal-content
      (let [{:keys [id header img title descr link date] :as values} @values]
-      (mini-cert id header img title descr link date values modal))]]])
+       (mini-cert id header img title descr link date values modal))]]])
      ;; (full-cert-footer modal values)]]])
